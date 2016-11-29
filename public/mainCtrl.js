fad6bc65cc1c5baa51074b3ac44f0ecd773c81f1
Rich <mtclimber82@yahoo.com>
2016-11-29 15:24:32 -0700

    Tweaked code to get Game 1 functional

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -61,19 +61,312 @@ $scope.states = [{
         landmarks: 'This state is home to Yellostone National Park'
     },
     id: 8
-}];
+} ,{
+    name: 'Colorado',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 9
+} ,{
+    name: 'Utah',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 10
+} ,{
+    name: 'New Jersey',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 11
+} ,{
+    name: 'New York',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 12
+} ,{
+    name: 'New Hampshire',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 13
+} ,{
+    name: 'Connecticut',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 14
+} ,{
+    name: 'North Dakota',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 15
+} ,{
+    name: 'South Dakota',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 16
+} ,{
+    name: 'Indiana',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 17
+} ,{
+    name: 'Illinois',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 18
+} ,{
+    name: 'Missouri',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 19
+} ,{
+    name: 'Louisianna',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 20
+} ,{
+    name: 'Oregon',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 21
+} ,{
+    name: 'Idaho',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 22
+} ,{
+    name: 'New Mexico',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 23
+} ,{
+    name: 'Texas',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 24
+} ,{
+    name: 'Alabama',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 25
+} ,{
+    name: 'Georgia',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 26
+} ,{
+    name: 'Virginia',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 27
+} ,{
+    name: 'North Carolina',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 28
+} ,{
+    name: 'South Carolina',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 29
+} ,{
+    name: 'Tennessee',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 30
+} ,{
+    name: 'Kentucky',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 31
+} ,{
+    name: 'Arkansas',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 32
+} ,{
+    name: 'Mississippi',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 33
+} ,{
+    name: 'Oklahoma',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 34
+} ,{
+    name: 'Nebraska',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 35
+} ,{
+    name: 'Wisconsin',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 36
+} ,{
+    name: 'Minnesota',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 37
+} ,{
+    name: 'Maine',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 38
+} ,{
+    name: 'Pennsylvania',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 39
+} ,{
+    name: 'West Virginia',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 40
+} ,{
+    name: 'Maryland',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 41
+} ,{
+    name: 'Massachusetts',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 42
+} ,{
+    name: 'Vermont',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 43
+} ,{
+    name: 'Rhode Island',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 44
+} ,{
+    name: 'Michigan',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 45
+} ,{
+    name: 'Nevada',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 46
+} ,{
+    name: 'Wyoming',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 47
+} ,{
+    name: 'Florida',
+    questions: {
+        capital: 'Helena',
+        landmarks: 'This state is home to Yellostone National Park'
+    },
+    id: 48
+}
+];
+
+//stop game after 60 seconds
+//add AK & HI
 
 $scope.question =
   function(){
-      var state = $scope.states[Math.floor(Math.random()*8)].name;
+      var state = "";
+      score = 0;
+      function setState(){
+        state = $scope.states[Math.floor(Math.random()*48)].name;
+        $('.questionBox').html("Click on the state of " + state);
+        $('.points').html('Score: ' + score);
+        score++;
+      };
+      state = $scope.states[Math.floor(Math.random()*48)].name;
       $('.questionBox').html("Click on the state of " + state);
-      $('polygon').off().on('click', function(e){
+      $('polygon, path').off().on('click', function(e){
         console.log(e.target.id);
         if(e.target.id == state){
-          alert('Correct!');
+          console.log('Correct!');
+          setState();
         } else {
-          alert('Oops...try again.');
+          console.log('Oops...try again.');
         };
-      })
+      });
     };
 });
